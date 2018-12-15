5401d077e20122eefb621c1d288501f5e466194c
srikanthpragada <srikanthpragada@gmail.com>
2018-12-15 08:28:04 +0530

    15-DEC

---
+++ ex/even_odd.py
@@ -0,0 +1,16 @@
+nums = []  # Empty list
+
+while True:
+    num = int(input("Enter a number [0 to stop] :"))
+    if num == 0:
+        break
+
+    if num % 2 == 0:
+        nums.insert(0,num)
+    else:
+        nums.append(num)
+
+for n in nums:
+    print(n, end=' ')
+
+
---
+++ funs/simple.py
@@ -0,0 +1,15 @@
+def fun():
+    print("Having fun!")
+
+
+def add(a=1, b=1):
+    print(a + b)
+
+
+fun()
+add(10, 20)
+add('A', 'B')
+# add(10,'A')
+add(10)
+add()
+
